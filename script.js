d5c1e95df52cc818e2f85dd2b47653a0aae332e5
unknown <mGitM101@gmail.com>
2023-01-02 17:26:43 +0100

    adding js file

---
+++ script.js
@@ -0,0 +1,5 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e1af1b3183ad43eb47034e6484afa4f4&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_URL =
+  'https://api.themoviedb.org/3/search/movie?api_key=e1af1b3183ad43eb47034e6484afa4f4&query="';
